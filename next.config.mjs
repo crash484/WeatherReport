028447c4b00335073253df60987eff4c4122740a
crash484 <awesomedude019@gmil.com>
2024-12-06 16:37:50 +0530

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,  // Optional, but recommended
-  };
-  
-  module.exports = nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
